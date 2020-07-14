d41b2d59d3e85ce1e1dd26b962e81ddcc3ae49ec
reto-a <jesus.galarza@banregio.com>
2020-07-14 10:09:25 -0500

    folder name change

--- scully.ngscully-netlify-cms-starter.config.ts
+++ scully.ngscully-netlify-cms-starter.config.ts
@@ -1,7 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: './src',
-  projectName: 'ngscully-netlify-cms-starter',
+  projectName: 'angular-example',
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
